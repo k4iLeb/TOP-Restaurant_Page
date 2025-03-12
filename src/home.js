b9a5a0e10043703a97426f7cbe5cb9e501666a1d
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-03-12 17:38:22 +0200

    First commit, setup webpack, link html,css,js

---
+++ src/home.js
@@ -0,0 +1,21 @@
+// The now multi-award-winning Jackaroo is the absolute must-try in town.
+// It's already smashing all order records, while outside the stores, there's a permanent street party going on!
+
+// HOURS
+// Tuesday 6pm – 1am
+// Wednesday 6pm – 1am
+// Thursday 6pm – 1am
+// Friday 6pm – 1am
+// Saturday 12.30pm–1am
+// Sunday 12.30pm–1am
+
+hourArr = [
+  "Tuesday 6pm – 1am",
+  "Wednesday 6pm – 1am",
+  "Thursday 6pm – 1am",
+  "Friday 6pm – 1am",
+  "Saturday 12.30pm–1am",
+  "Sunday 12.30pm–1am",
+];
+
+// Contact
---
+++ src/menus.js
@@ -0,0 +1,5 @@
+// Title
+
+// Category Name
+
+// Cards
