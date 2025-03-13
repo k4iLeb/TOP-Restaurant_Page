30f4f6afd3d6b3cee6c2f595135324f8e4cc5a90
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-03-13 17:52:18 +0200

    organize assets in seperate folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,15 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    assetModuleFilename: (pathData) => {
+      const ext = path.extname(pathData.filename).toLowerCase();
+      if ([".woff", ".woff2", ".eot", ".ttf", ".otf"].includes(ext)) {
+        return "fonts/[hash][ext][query]";
+      } else if ([".png", ".svg", ".jpg", ".jpeg", ".gif"].includes(ext)) {
+        return "images/[hash][ext][query]";
+      }
+      return "[hash][ext][query]"; //default case if it is neither font nor image.
+    },
   },
   devtool: "eval-source-map",
   devServer: {
